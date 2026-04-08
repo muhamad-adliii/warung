6ec36f287bd3b19b354469c9c0495e0f355a1c16
muhamad adli <muhmdadli01@gmail.com>
2026-04-08 22:25:35 +0700

    data barang progress

--- resources/views/dashboard/data_barang.blade.php
+++ resources/views/dashboard/data_barang.blade.php
@@ -2,6 +2,15 @@
 
 @section('content')
     <div class="">
-        
+        <div>
+            <button
+                class="py-2 bg-[#5d5de7] w-full font-medium cursor-pointer hover:bg-[#4040e8] flex gap-2 text-[#ffffff] capitalize rounded-lg md:w-auto justify-center px-10 text-md">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                </svg>
+                add student
+            </button>
+        </div>
     </div>
 @endsection
